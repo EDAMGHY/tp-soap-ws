155eac76fe0e78c2f8c3b13c3898c72baf679cfd
Abdellah DAMRI <damriabdellah@gmail.com>
2024-01-02 22:19:15 +0100

    Add: adding the server side

---
+++ server/src/main/java/ws/BanqueService.java
@@ -0,0 +1,31 @@
+package ws;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebService;
+
+import java.util.Date;
+import java.util.List;
+
+@WebService(serviceName = "BanqueService")
+public class BanqueService {
+    @WebMethod(operationName = "convertEuroToDh")
+    public double convertEuroToDh(@WebParam(name = "montant") double montant) {
+        return montant * 11;
+    }
+    @WebMethod(operationName = "getCompte")
+
+    public Compte getCompte(@WebParam(name = "code") int code) {
+        return new Compte(code, Math.random() * 100000, new Date());
+    }
+    @WebMethod(operationName = "listComptes")
+
+    public List<Compte> listComptes() {
+        return List.of(
+                new Compte(1, Math.random() * 100000, new Date()),
+                new Compte(2, Math.random() * 100000, new Date()),
+                new Compte(3, Math.random() * 100000, new Date())
+        );
+    }
+}
+
